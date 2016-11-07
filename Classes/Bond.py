29651781f03813f986dcb8a9f1170f19cc56b4cc
Jacob Casper <jacob.thomas.casper@gmail.com>
2016-11-06 23:53:08 -0500

    Implemented Bond ID and API methods

--- Classes/Bond.py
+++ Classes/Bond.py
@@ -6,11 +6,12 @@ import AGG
 
 class Bond(AGG.Classes.API.Arc):
 
-    def __init__(self, atom_ID1, atom_ID2, bondOrder):
+    def __init__(self, atom_ID1, atom_ID2, bondOrder, ID=None):
 
         self.atom_ID1 = atom_ID1
         self.atom_ID2 = atom_ID2
         self.bondOrder = bondOrder
+        self.ID = ID;
 
     #-----------------
     # Bond Accessors
@@ -20,7 +21,7 @@ class Bond(AGG.Classes.API.Arc):
         """
         Get both atom_ids of this bond
         """
-        return (self.getAtom_ID2(), self.getAtom_ID1())
+        return (self.getAtom_ID1(), self.getAtom_ID2())
 
     def getAtom_ID2(self):
         """
@@ -41,6 +42,12 @@ class Bond(AGG.Classes.API.Arc):
         """
         return self.bondOrder
 
+    def getID(self):
+        """
+        Get Bond ID
+        """
+        return self.ID;
+
 
     # ----------------------------------------
     # API interface methods.
@@ -54,45 +61,35 @@ class Bond(AGG.Classes.API.Arc):
           nor any guarantee of global uniqueness.  The
           ID should be unique within the graph, however.
         """
-        raise APIError, "Unimplemented Stub."
+        return self.getID();
 
 
     def agg_getNodes(self):
         """
         Get the start and end nodes.
         """
-        raise APIError, "Unimplemented Stub."
+        return self.getAtoms();
 
 
     def agg_getStartNode(self):
         """
         Get the start node for this arc.
         """
-        raise APIError, "Unimplemented Stub."
+        return self.getAtom_ID1
 
 
     def agg_getEndNode(self):
         """
         Get the end node for this arc.
         """
-        raise APIError, "Unimplemented Stub."
-
-
-    # def agg_isDirected(self):
-    #     """
-    #     Return a boolean indicating whether this arc is
-    #     directed or undirected.  If directed it should
-    #     start at the Start node and end at the End node.
-    #     """
-    #     raise APIError, "Unimplemented Stub."
-
+        return self.getAtom_ID2();
 
     def agg_getType(self):
         """
         Get the type of this arc as per the ontology
         set in the system.
         """
-        raise APIError, "Unimplemented Stub."
+        return self.getOrder();
 
 
     def agg_hasField(self, FieldName, Subfield=None):
